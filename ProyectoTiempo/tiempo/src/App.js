959326976973850ebfb9fd8e3b077970380f0c8b
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2023-07-03 23:00:59 +0200

    EmpezandoProyectoAñadiendoBoostrapYcreandoBarraNavegacion

---
+++ ProyectoTiempo/tiempo/src/App.js
@@ -0,0 +1,15 @@
+import '../src/hojasdeestilo/App.css';
+import BarraNavegacion from './componentes/BarraNavegacion';
+
+function App() {
+  return (
+    <div className="App">
+      
+    <BarraNavegacion />
+
+
+    </div>
+  );
+}
+
+export default App;
---
+++ ProyectoTiempo/tiempo/src/componentes/BarraNavegacion.js
@@ -0,0 +1,17 @@
+const BarraNavegacion = () => {
+    return (
+
+        <nav className="navbar bg-dark text-light mb-5" >
+
+            <div className="container-fluid">
+                <h3 className="mx-auto">Prediccion meteorologica</h3>
+
+            </div>
+
+        </nav>
+
+
+    );
+}
+
+export default BarraNavegacion;
