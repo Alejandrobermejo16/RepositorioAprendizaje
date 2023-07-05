e25b6e8f1937ca6f52e5f694c20e5ed5aeabc653
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2023-07-05 16:31:27 +0200

    aplicacion-terminada

--- ProyectoTiempo/tiempo/src/componentes/Tarjeta.jsx
+++ ProyectoTiempo/tiempo/src/componentes/Tarjeta.jsx
@@ -12,8 +12,19 @@ var año = hoy.getFullYear();
 //todo junto 
 var fecha = dia + '/' + mes + '/' + año;
 
+//para obtener la llamada al icono
 var url = "";
 var iconUrl = "";
+//la api muestra los resultados cada 3 horas
+var iconUrl3 = "";
+var iconUrl6 = "";
+var iconUrl9 = "";
+
+
+var forecastfecha3 = ""; //la fecha de 3/6/9 horas despues
+var forecastfecha6 = "";
+var forecastfecha9 = "";
+
 
     if (loadingData) {   //si esta buscando el dato carga el spinner sino ya sale el return de la tarjeta
         //es decir, el spinner solo sale cuando esta vacia la tarjeta buscando el dato
@@ -21,8 +32,22 @@ var iconUrl = "";
     }
 
     if (showData){  //Elegimos cuando se muestran los datos por que sino puede darnos fallo
-      url = "http://openweathermap.org/img/w/" //url del icono de la api
-      iconUrl = url + tiempo.weather[0].icon + ".png"; //weather porque en la api viene de weather
+      
+        url = "http://openweathermap.org/img/w/" //url del icono de la api
+      
+        iconUrl = url + tiempo.weather[0].icon + ".png"; //weather porque en la api viene de weather
+      
+      iconUrl3 = url + forecast.list[1].weather[0].icon + ".png"; //queremos poner el tiempo de las proximas horas
+      iconUrl6 = url + forecast.list[2].weather[0].icon + ".png";
+      iconUrl9 = url + forecast.list[3].weather[0].icon + ".png";
+
+      forecastfecha3 = forecast.list[1].dt_txt.substring(8, 10) + '/' + forecast.list[1].dt_txt.substring(5, 7) + '/' +  forecast.list[1].dt_txt.substring(0, 4) + ' ' + forecast.list[1].dt_txt.substring(11, 13);
+      forecastfecha6 = forecast.list[2].dt_txt.substring(8, 10) + '/' + forecast.list[2].dt_txt.substring(5, 7) + '/' +  forecast.list[2].dt_txt.substring(0, 4) + ' ' + forecast.list[2].dt_txt.substring(11, 13);
+      forecastfecha9 = forecast.list[3].dt_txt.substring(8, 10) + '/' + forecast.list[3].dt_txt.substring(5, 7) + '/' +  forecast.list[3].dt_txt.substring(0, 4) + ' ' + forecast.list[3].dt_txt.substring(11, 13);
+
+
+      //obtenemos con la funcion los caracteres , en este caso la hora el mes y el dia y los dos caracteres de la hora 
+
     }
 
     return (
@@ -48,11 +73,36 @@ var iconUrl = "";
                                     </div>
                                     <div className="col-md-8">
                                         <div className="card-body text-start mt-2">
+                                                               {/*Se le restan los grados farenheit */}
                                             <h5 className="card-text">Temperatura maxima: {(tiempo.main.temp_max - 273.15).toFixed(1)}Cº </h5>
                                             <h5 className="card-text">Temperatura minima: {(tiempo.main.temp_min - 273.15).toFixed(1)}Cº </h5>
                                             <h5 className="card-text">Sensacion Termica: {(tiempo.main.feels_like - 273.15).toFixed(1)}Cº </h5>
+                                            <h5 className="card-text"> Humedad : {tiempo.main.humidity}% </h5>
+                                            <h5 className="card-text"> Velocidad del viento : {tiempo.wind.speed}m/s </h5>
+                                            <hr/>
+
+                                                <div className="row mt-4">
+
+                                                <div className="col"> {/*para la fecha*/}
+                                                 <p>{forecastfecha3}h</p>
+                                                 <p className="description"><img src={iconUrl3} alt="icon" />{forecast.list[1].weather[0].description} </p>
+                                                 <p className="temp"> {(forecast.list[1].main.temp -273.15).toFixed(1)}ºC </p>
+                                                </div>
+                                                <div className="col">
+                                                <p>{forecastfecha6}h</p>
+                                                 <p className="description"><img src={iconUrl6} alt="icon" />{forecast.list[2].weather[0].description} </p>
+                                                 <p className="temp"> {(forecast.list[2].main.temp -273.15).toFixed(1)}ºC </p>
+
+                                                </div>
+                                                <div className="col">
+                                                <p>{forecastfecha9}h</p>
+                                                 <p className="description"><img src={iconUrl9} alt="icon" />{forecast.list[3].weather[0].description} </p>
+                                                 <p className="temp"> {(forecast.list[3].main.temp -273.15).toFixed(1)}ºC </p>
+                                                </div>
 
 
+
+                                                </div>
                                         </div>
                                     </div>
                                 </div>
