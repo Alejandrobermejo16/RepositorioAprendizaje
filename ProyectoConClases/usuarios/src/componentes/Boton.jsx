d52c4cb68ba91e1d60031fcad1afe4c2b2d9af92
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2023-07-08 12:18:16 +0200

    trabajamos-con-clases-para-poder-trabajar-con-versiones-desactualizadas-de-react

---
+++ ProyectoConClases/usuarios/src/componentes/Boton.jsx
@@ -0,0 +1,30 @@
+import React, { Component } from 'react';
+
+class Boton extends Component {
+  
+  constructor (props){
+    super(props);
+     this.state = {  //estado inicial antes de mostrarse en el DOM
+      texto: '',
+    };
+     console.log(this.state.texto,"soy constructor del padre, antes de pintar ");
+     //aqui da igual poner algo que no , porque no se va a reenderizar
+    }
+
+  render() {
+
+       const {texto} = this.props; 
+       //para poder utilizar la prop sin referenciar this.props en el return
+
+    return (
+
+              <button>{texto}</button>  
+              //definimos una prop que estará en un estado inicial '' y definiremos cuando llamemos al componente en un hijo
+              //pero este que es el padre , no tocara el estado inicial porque permanece inmutable 
+    );
+  }
+ }
+
+export default Boton;
+
+
---
+++ ProyectoConClases/usuarios/src/componentes/RecuperaDatos.jsx
@@ -0,0 +1,19 @@
+import React, { useState } from "react";
+import Boton from './Boton';
+
+const RecuperaDatos = () =>{
+
+  
+
+
+
+
+ return(
+
+    <Boton></Boton>
+
+ );
+
+}
+
+export default RecuperaDatos;
