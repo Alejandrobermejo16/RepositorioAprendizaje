afcac89dbeb50909da321946d50d422b8aa6b5c2
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2023-07-21 10:36:27 +0200

    proyectos-entendiendo-apis-y-plantillas-literales

---
+++ ProyectoMetodosParaLlamarApis/llamadasapis/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Llamada1 from './componentes/Llamada1';
+import PokemonInfo from './componentes/Llamada2ConProps';
+
+function App() {
+  return (
+    <div className="App">
+      <Llamada1 />
+      <PokemonInfo />
+  
+    </div>
+  );
+}
+
+export default App;
---
+++ ProyectoMetodosParaLlamarApis/llamadasapis/src/componentes/Llamada1.jsx
@@ -0,0 +1,25 @@
+//ponemos lo basico para entender como se hace una llamada a la api
+
+
+
+const Llamada1 = () => {
+    const llamarApi = () => {
+      return fetch("https://pokeapi.co/api/v2/pokemon/charizard")       //hacemos la llamada a la api y a un objeto en cuestion
+        .then(respuesta => respuesta.json()); //convertimos la respuesta en json para que sea legible
+    }
+  
+    llamarApi()  //llamamos al metodo y le ponemos then para que espere hasta que carguen los datos
+               //y mostramos por pantalla los datos completos, pero se puede llamar a cualquier dato del array
+               //para futuramente mostrarlo en cualquier elemento del dom pj datos.name
+      .then(datos => console.log("nombre del pokemon",datos.name,"objeto completo a la derecha",datos))
+      //en caso de dar un error la peticion de datos sacara el error por consola
+      .catch(error => console.error("Error al obtener datos:", error));
+  }
+
+  
+  
+  export default Llamada1;
+
+
+
+  
