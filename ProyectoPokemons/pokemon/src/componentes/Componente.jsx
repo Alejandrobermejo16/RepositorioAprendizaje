58a72a24561f8be0dbffb1262237996bd9b88ec3
Alejandrobermejo16 <alejandrobermejomendez170712@gmail.com>
2023-08-17 13:10:18 +0200

    Cambios-animacion

--- ProyectoPokemons/pokemon/src/componentes/Componente.jsx
+++ ProyectoPokemons/pokemon/src/componentes/Componente.jsx
@@ -4,66 +4,88 @@ class Llamada2 extends Component {
   constructor(props) {
     super(props);
     this.state = {
-    pokemonData: null,
-    showAnimation: false, // Estado para activar la animación
-    showRetryButton: false, // Estado para mostrar el botón de reintentar
-  };
-}
-
-handleButtonClick = async () => {
-  try {
-    const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/charizard");
-    const datos = await respuesta.json();
-    console.log(datos.name);
-
-    this.setState({ pokemonData: datos, showAnimation: true }); // Activamos la animación
-
-    // Agregamos un pequeño retraso para mostrar el botón de reintentar después de la animación
-    setTimeout(() => {
-      this.setState({ showRetryButton: true });
-    }, 500); // Ajusta el valor del retraso según sea necesario
-  } catch (error) {
-    console.error("Error al obtener datos:", error);
+      pokemonData: null,
+      showAnimation: false,
+      showRetryButton: false,
+      isFirstClick: true,
+    };
   }
-};
 
-handleRetryClick = async () => {
-  await this.resetAnimation(); // Reiniciar la animación
-  setTimeout(() => {
+  handleButtonClick = async () => {
+    const nombrePokemon = [
+      'charizard',
+      'pikachu',
+      'ditto',
+      'fearow',
+      'meowth',
+      'blastoise',
+      'persian',
+    ];
+
+    // Generar un índice aleatorio
+    const indiceAleatorio = Math.floor(Math.random() * nombrePokemon.length);
+
+    // Obtener el nombre de Pokémon aleatorio usando el índice aleatorio
+    const nombreAleatorio = nombrePokemon[indiceAleatorio];
+
+    try {
+      // Mostrar el fondo negro durante medio segundo en el segundo clic
+      if (!this.state.isFirstClick) {
+        this.setState({ showAnimation: false });
+        await new Promise((resolve) => setTimeout(resolve, 500));
+      }
+
+      const respuesta = await fetch(`https://pokeapi.co/api/v2/pokemon/${nombreAleatorio}`);
+      const datos = await respuesta.json();
+      console.log(datos.name);
+
+      this.setState({ pokemonData: datos, showAnimation: true, isFirstClick: false });
+
+      // Agregamos un pequeño retraso para mostrar el botón de reintentar después de la animación
+      setTimeout(() => {
+        this.setState({ showRetryButton: true });
+      }, 500);
+    } catch (error) {
+      console.error("Error al obtener datos:", error);
+    }
+  };
+
+  handleRetryClick = async () => {
+    this.setState({ showAnimation: false, showRetryButton: false, isFirstClick: true });
+    await this.resetAnimation();
     this.handleButtonClick();
-  }, 10);
-};
+  };
 
-resetAnimation = async () => {
-  this.setState({ showAnimation: false, pokemonData: null, showRetryButton: false });
-  await new Promise((resolve) => setTimeout(resolve, 10)); // Esperar un corto tiempo para asegurarse de que el estado se haya actualizado
-};
+  resetAnimation = async () => {
+    this.setState({
+      pokemonData: null,
+    });
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+  };
 
-render() {
-  const { pokemonData, showAnimation, showRetryButton } = this.state;
+  render() {
+    const { pokemonData, showAnimation, showRetryButton, isFirstClick } = this.state;
 
-  return (
-    <div className={`App ${showAnimation ? 'animate' : ''}`}>
-      {pokemonData && (
-        <h3 className={`result ${showAnimation ? 'active' : ''}`}>
-          {pokemonData.name}
-        </h3>
-      )}
-      {!showAnimation && !showRetryButton && (
-        <button className="btn" onClick={this.handleButtonClick}>
-          Pulsa para ver el pokemon
-        </button>
-      )}
-      {showRetryButton && (
-        <button className="retry-btn" onClick={this.handleRetryClick}>
-          Prueba de nuevo
-        </button>
-      )}
-    </div>
-  );
-}
+    return (
+      <div className={`App ${showAnimation ? 'animate' : ''}`}>
+        {showAnimation && pokemonData && !isFirstClick && (
+          <h1 className={`result ${showAnimation ? 'active' : ''}`}>
+            {pokemonData.name}
+          </h1>
+        )}
+        {!showAnimation && !showRetryButton && (
+          <button className="btn" onClick={this.handleButtonClick}>
+            {isFirstClick ? "Pulsa para ver el pokemon" : "Pulsa para repetir"}
+          </button>
+        )}
+        {showRetryButton && showAnimation && (
+          <button className={`retry-btn ${showAnimation ? 'active' : ''}`} onClick={this.handleRetryClick}>
+            Prueba de nuevo
+          </button>
+        )}
+      </div>
+    );
+  }
 }
 
 export default Llamada2;
-
-
